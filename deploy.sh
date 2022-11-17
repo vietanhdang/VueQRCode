16cbd4243b3c54178c65bbb9d948e8930df1bbfe
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2022-11-17 16:32:54 +0700

    up

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:manolisliolios/easy-qr.git master:gh-pages
+git push -f git@github.com:vietanhdang/easy-qr.git master:gh-pages
 
 cd -
